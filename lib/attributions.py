85b84cca753e1d91e1f1d6adbe8b523ccbedbb2b
Maciej Satkiewicz <msatkiewicz@gmail.com>
2025-12-22 15:16:27 +0100

    standard sanity checks in notebook

--- lib/attributions.py
+++ lib/attributions.py
@@ -19,12 +19,15 @@ class LocalGradientAscent:
 
 
 class LocalRelevanceAscent(LocalGradientAscent):
+    # NOTE: This modifies the model IN PLACE, but should not affect forward nor backward passes, as we leave standard_backward=True
     def __init__(self, model, temperatures, alpha=None, steps=1, eps=None):
         super().__init__(model, alpha=alpha, steps=steps, eps=eps)
 
         self.temperatures = temperatures
         soften_module_inplace_(self.model, temperatures=self.temperatures)
-        set_module_standard_backward_(self.model, standard_backward=True)
+        set_module_standard_backward_(
+            self.model, standard_backward=True
+        )  # don't change the model behavior!
 
     def attribute(self, inputs, target):
         set_module_standard_backward_(self.model, standard_backward=False)
